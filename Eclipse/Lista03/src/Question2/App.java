3f59e5a76ed4a3651f77564b5ed6d19f47dce1d3
lbsousa29 <lb157866@gmail.com>
2025-08-31 22:33:50 -0300

    Atualizei

---
+++ Eclipse/Lista03/src/Question2/App.java
@@ -0,0 +1,20 @@
+package Question2;
+
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+	
+	Scanner read = new Scanner(System.in);
+	System.out.println("Digite seu nome:" );
+	String setNome = read.nextLine(); 
+	System.out.println("Digite seu sálario:");
+	Double setSalario = read.nextDouble();
+	Funcionario  F = new  Funcionario(setNome, setSalario);
+	String nome = null;
+	F.setNome(nome);
+	double salario = 0;
+	F.setSalario(salario);
+	System.out.println("Imposto do seu salario é " + F.calcularIrpf(setSalario) );
+}
+ }
--- Eclipse/Lista03/src/Question2/Funcionario.java
+++ Eclipse/Lista03/src/Question2/Funcionario.java
@@ -1,4 +1,4 @@
-package Question1;
+package Question2;
 
 public class Funcionario {
 	private String nome;
@@ -10,11 +10,6 @@ public class Funcionario {
 	}
 	
 	
-
-	public Funcionario() {
-	}
-
-
 
 	public String getNome() {
 		return nome;
@@ -28,7 +23,7 @@ public class Funcionario {
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-	public double calcularIrpf() {
+	public double calcularIrpf(double salario) {
 		
 		if (salario <= 1903.98) {
 		
@@ -53,6 +48,5 @@ public class Funcionario {
 		
 		return salario;
 	}
-	
 	
 }
